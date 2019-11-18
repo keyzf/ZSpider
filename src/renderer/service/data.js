2e7746fe9ebe0262fbe3df98c305ec14727c7a24
klkl <kuanglong0312@sina.com>
2019-11-18 20:32:58 +0800

    feat: db

--- src/renderer/service/data.js
+++ src/renderer/service/data.js
@@ -1,6 +1,13 @@
-const sqlite = require('sqlite3')
-
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('spider')
 export async function setData (appid, data) {
+  db.serialize(() => {
+    db.run(`CREATE TABLE data-${appid} (
+      column_name1 data_type(size),
+      column_name2 data_type(size),
+      column_name3 data_type(size),
+    )`)
+  })
   try {
   } catch (e) {}
 }
